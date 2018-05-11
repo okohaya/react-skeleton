6ff787811ce57ebd1c0494e49ea870f7ccb2246e
okohaya <okohaya@gmail.com>
2018-05-11 22:46:30 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const root = document.createElement('div')
+root.id = 'root'
+document.body.appendChild(root)
+
+ReactDOM.render(
+  <div>hello world</div>,
+  root
+)
