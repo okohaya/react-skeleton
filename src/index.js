c015c11c054389eeeb19f05d075d646161df2d80
okohaya <okohaya@gmail.com>
2018-05-16 12:56:45 +0900

    Use hot module replacement

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './components/App'
 
 const root = document.createElement('div')
 root.id = 'root'
 document.body.appendChild(root)
 
 ReactDOM.render(
-  <div>hello world</div>,
+  <App />,
   root
 )
